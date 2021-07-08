bf797b3913199c8793fdddd1fb1536b37f3486b0
Lucifer2op <87086143+Lucifer2op@users.noreply.github.com>
2021-07-08 22:32:37 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -29,7 +29,7 @@ async def ADRINO(client,message):
 value  = {'long_url': URL , 'domain': DOMAIN}
 data = json.dumps(value)
   try:
-r = requests.Get('https://adrinolinks.in/api?api=ADRINO_TOKEN&url=', headers=headers,data = data )
+    r = requests.Get('https://adrinolinks.in/api?api=ADRINO_TOKEN&url=', headers=headers,data = data )
     result = r.json()
     link = result["link"]
     await message.reply_text(f"```{link}```", reply_to_message_id= message.message_id)
