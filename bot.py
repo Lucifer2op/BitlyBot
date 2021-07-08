62b673ee4467bfbd38e77b912b1f324c2eded360
Lucifer2op <87086143+Lucifer2op@users.noreply.github.com>
2021-07-08 21:00:32 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -25,8 +25,7 @@ URL = message.text
 DOMAIN = "adrinolinks.in"
 value  = {'long_url': URL , 'domain': DOMAIN}
 data = json.dumps(value)
-  try:
-    r = requests.Get('https://adrinolinks.in/api?api=ADRINO_TOKEN&url=', headers=headers,data = data )
+ r = requests.Get('https://adrinolinks.in/api?api=ADRINO_TOKEN&url=', headers=headers,data = data )
     result = r.json()
     link = result["link"]
     await message.reply_text(f"
