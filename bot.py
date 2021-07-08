49567ed26242f30ea2d899c1786fc1dd2bd8afe2
Lucifer2op <87086143+Lucifer2op@users.noreply.github.com>
2021-07-08 13:36:08 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,16 +21,13 @@ app = Client("pdiskbot" ,bot_token = TOKEN ,api_id = API_ID ,api_hash = API_HASH
 @app.on_message(filters.private & filters.command(['start']))
 async def start(client,message):
   await message.reply_text(f"Hello {message .from_user.first_name}\nhello i am adrinolinks.in short link genrator\n made with love by @Lucifer_morning_star_op ", reply_to_message_id = message.message_id)
-  URL = message.text
-  DOMAIN = "adrinolinks.in"
-  value  = {'long_url': URL , 'domain': DOMAIN}
-  data = json.dumps(value)
-  try:
-    r = requests.post('https://adrinolinks.in/api?api=ADRINO_TOKEN&url=', headers=headers,data = data )
-    result = r.json()
-    link = result["link"]
-    await message.reply_text(f"```{link}```", reply_to_message_id= message.message_id)
-  except Exception as e :
-    await message.reply_text(e)
+$long_url = urlencode('yourdestinationlink.com');
+$api_url = "https://adrinolinks.in/api?api={$ADRINO_TOKEN}&url={$long_url}&alias=CustomAlias";
+$result = @json_decode(file_get_contents($api_url),TRUE);
+if($result["status"] === 'error') {
+ echo $result["message"];
+} else {
+ echo $result["shortenedUrl"];
+}
   
 app.run()
